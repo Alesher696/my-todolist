c423b46dbea7afa4d22cf4545bd98f7025761512
Aleksandr Sherstnev <alesher696@gmail.com>
2023-01-03 17:05:42 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
-import Todolist from './todolist';
-import {TaskType} from "./todolist";
-function App() {
-    const todoList_1: string = "What to learn?";
-    const todoList_2: string = "What to buy";
-    const tasks_1: Array<TaskType> = [
-        {id: 1, title: 'HTML' , isDone:true},
-        {id: 2, title: 'TS', isDone: true},
-        {id: 3, title: 'JS', isDone:true}]
-    return (
-        <div className="App">
-            <Todolist title={todoList_1} tasks = {tasks_1}/>
+import {TaskType, Todolist} from "./todolist";
 
-        </div>
-    );
+
+export const App = () => {
+    const title_1: string = "What's learn?"
+    const title_2: string = "What's buy?"
+    const task_1: Array<TaskType> = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'TS', isDone: true},
+        {id: 3, title: 'JS', isDone: true},
+        {id: 4, title: 'JSX', isDone: true}
+    ]
+    const task_2: Array<TaskType> = [
+        {id: 1, title: 'tomato', isDone: false},
+        {id: 2, title: 'potato', isDone: false},
+        {id: 3, title: 'meat', isDone: false}
+    ]
+    return (<div className='App'>
+
+        <Todolist title={title_1} tasks={task_1}/>
+        <Todolist title={title_2} tasks={task_2}/>
+    </div>)
+
 }
 
-export default App;
+
+// import Todolist from './todolist';
+// import {TaskType} from "./todolist";
+// function App() {
+//     const todoList_1: string = "What to learn?";
+//     const todoList_2: string = "What to buy";
+//     const tasks_1: Array<TaskType> = [
+//         {id: 1, title: 'HTML' , isDone:true},
+//         {id: 2, title: 'TS', isDone: true},
+//         {id: 3, title: 'JS', isDone:true}]
+//     return (
+//         <div className="App">
+//             <Todolist title={todoList_1} tasks = {tasks_1}/>
+//
+//         </div>
+//     );
+// }
+//
+// export default App;
